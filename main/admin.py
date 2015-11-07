1afa28eec301c7451bc9f06f11db33866ae448ce
林浩特 <lnhote@gmail.com>
2015-11-07 22:27:05 +0800

    后台也得弄的好看些。

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import *
 
-admin.site.register(News)
-admin.site.register(Asset)
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    fields = ('title', 'desc', 'content')
+    list_display = ('title', 'created', 'modified')
+    list_filter = ('created',)
+    date_hierarchy = 'created'
--- main/models.py
+++ main/models.py
@@ -6,11 +6,11 @@ import datetime
 
 # Create your models here.
 class News(models.Model):
-    title = models.CharField(max_length=50, default="标题")
-    desc = models.CharField(max_length=255, default="描述")
-    content = RichTextUploadingField()
-    created = models.DateTimeField()
-    modified = models.DateTimeField()
+    title = models.CharField(max_length=50, default="标题", verbose_name=u"文章标题")
+    desc = models.CharField(max_length=255, default="描述", verbose_name=u"文章描述")
+    content = RichTextUploadingField(verbose_name=u"正文")
+    created = models.DateTimeField(auto_now_add=True, verbose_name=u"发布时间")
+    modified = models.DateTimeField(auto_now=True, verbose_name=u"修改时间")
 
     def __unicode__(self):
         return self.title
@@ -18,6 +18,10 @@ class News(models.Model):
     def was_created_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=30) <= self.created <= now
+
+    class Meta:
+        verbose_name = '资讯'
+        verbose_name_plural = '资讯'
 
 class Asset(models.Model):
     url = models.TextField(default="")
