b01e184c72fb818d9ccf2bd7091ed8845433dae1
林浩特 <lnhote@gmail.com>
2015-11-10 13:57:33 +0800

    shell script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+WEB_PATH='/srv/www/tangbofu'
+WEB_USER='www-data'
+WEB_USERGROUP='www-data'
+ 
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+#git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
