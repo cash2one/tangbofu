a04b8adfe234eaad07c8bff0eccda17b29b52723
林浩特 <lnhote@gmail.com>
2015-11-10 14:34:08 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ WEB_USERGROUP='www-data'
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git reset --hard origin/master
+#git reset --hard origin/master
 #git clean -f
 git pull 
-git checkout master
-echo "changing permissions..."
+#git checkout master
+#echo "changing permissions..."
 #chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
 echo "Finished."
