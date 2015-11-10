86b1b74f9235ab3ebd73688ada442ef0fccba049
林浩特 <lnhote@gmail.com>
2015-11-10 14:32:46 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ git reset --hard origin/master
 git pull 
 git checkout master
 echo "changing permissions..."
-chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+#chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
 echo "Finished."
