d42c8cc70316e7f258956554a55a60ed4a75a843
林浩特 <lnhote@gmail.com>
2016-03-21 20:07:53 +0800

    add error page.

--- tangbofu/settings.py
+++ tangbofu/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SECRET['SECRET_KEY']
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
